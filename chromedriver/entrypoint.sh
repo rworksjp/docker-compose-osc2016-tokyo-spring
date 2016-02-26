e21941cacaec5b1f58a99e95420a3da010baede9
Akihiro Uchida <uchida@turbare.net>
2016-02-27 08:04:18 +0900

    make screensize for configurable

--- chromedriver/entrypoint.sh
+++ chromedriver/entrypoint.sh
@@ -2,5 +2,8 @@
 set -e
 
 rm -f /tmp/.X0-lock
+if [[ "$XVFB_SCREENSIZE" =~ ^[[:digit:]]{1,}x[[:digit:]]{1,}x(8|16|24)$ ]]; then
+  sed -i "s/1280x800x24/$XVFB_SCREENSIZE/" /etc/supervisor/conf.d/supervisord.conf
+fi
 
 exec "$@"
