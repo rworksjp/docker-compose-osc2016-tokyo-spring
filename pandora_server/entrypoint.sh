e51f9d046a637e73b4e1d2e6ba423a4974b03543
Akihiro Uchida <uchida@turbare.net>
2016-02-29 02:06:58 +0900

    squash/tidy layers and better package installation

--- pandora_server/entrypoint.sh
+++ pandora_server/entrypoint.sh
@@ -5,6 +5,7 @@ sed -i "s/^dbhost.*/dbhost mysql/" /etc/pandora/pandora_server.conf
 sed -i "s/^dbuser.*/dbuser $MYSQL_PANDORA_USER/" /etc/pandora/pandora_server.conf
 sed -i "s/^dbpass.*/dbpass $MYSQL_PANDORA_PASSWORD/" /etc/pandora/pandora_server.conf
 
-/register.pl
+chmod +x /opt/selenese/selenese-runner
+/usr/bin/perl /register.pl
 
 exec "$@"
